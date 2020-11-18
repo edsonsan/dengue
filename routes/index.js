3569ead6dbac539fe8f69b889162babf529d1d5b
Edson santos <erodrigues.santos@gmail.com>
2020-11-18 16:28:49 -0300

    Página de Cadastro, atualização das rotas para cadastro e inclusão de código Botão cadastro na página home

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
+});
+/* GET cadastro page */
+router.get('/cadastro', function(req,res,next) {
+  res.render('cadastro',{title: 'Cadastro'});
 });
 
 module.exports = router;
