92db6b2dcb2ecef1db549d32bcb538a4d7ceec2e
Edson santos <erodrigues.santos@gmail.com>
2020-12-03 14:47:31 -0300

    Inseria as telas usuário, denuncia-selecionada e atualizada as rotas

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('usuario/index',{title:'Home'});
 });
 
+/* GET denuncia listing. */
+router.get('/denuncia', function(req, res, next) {
+  res.render('usuario/denuncia',{title:'Denuncia'});
+});
+
+/* GET denuncia-selecionada listing. */
+router.get('/denuncia/:id', function(req, res, next) {
+  res.render('usuario/denuncia-selecionada',{title:'Denuncia Selecionada'});
+});
+
+/* GET usuario listing. */
+router.get('/usuario', function(req, res, next) {
+  res.render('usuario/usuario',{title:'Cadastro Usuário'});
+});
+
+
 module.exports = router;
